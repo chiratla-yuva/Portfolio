6805f382d2bf8f0eeafeeb13071563a6f2930c83
chiratla-yuva <chiratlayuva@gmail.com>
2024-08-15 12:06:53 +0530

    changes done

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -7,12 +7,14 @@ const Education = () => {
             <hr className="w-1/3 my-4 border-green-500" />
             <div className='rounded-3xl w-full min-h-max p-6 flex flex-col items-center gap-2.5'>
                 <div className='bg-gray-700 rounded-3xl w-full min-h-max p-6 flex flex-col items-center'>
-                    <h3 className='text-xl font-semibold'>B.Tech - CSE (Cyber Security)</h3>
+                    <h3 className='text-xl font-semibold'>UG</h3>
+                    <h3 className=''>B.Tech - CSE (Cyber Security)</h3>
                     <h4 className=''>GITAM University, Visakhapatnam</h4>
                     <h5 className=''>2021 - 2025</h5>
                 </div>
                 <div className='bg-gray-700 rounded-3xl w-full min-h-max p-6 flex flex-col items-center'>
                     <h3 className='text-xl font-semibold'>Intermediate</h3>
+                    <h3 className=''>Maths Physics Chemistry</h3>
                     <h4 className=''>Sri Chaitanya Junior College, Kakinada</h4>
                     <h5 className=''>2019 - 2021</h5>
                 </div>
