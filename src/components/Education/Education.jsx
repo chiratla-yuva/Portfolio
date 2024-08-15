ef57ae9b76b80a0c94f16f1b1754c4e95ff91702
chiratla-yuva <chiratlayuva@gmail.com>
2024-08-15 12:36:26 +0530

    changes done

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -6,20 +6,20 @@ const Education = () => {
             <h1 className="text-center text-4xl font-bold text-green-500">Education</h1>
             <hr className="w-1/4 my-4 border-green-500" />
             <div className='rounded-3xl w-full min-h-max p-6 flex flex-col items-center gap-2.5'>
-                <div className='bg-gray-700 rounded-3xl w-9/12 min-h-max p-4 flex flex-col items-center sm:w-full'>
-                    <h3 className='text-xl font-semibold'>B.Tech - CSE(Cyber Security)</h3>
+                <div className='bg-gray-700 rounded-3xl w-full min-h-max p-4 flex flex-col items-center lg:w-3/12'>
+                    <h3 className='text-xl font-semibold text-yellow-400'>B.Tech - CSE (Cyber Security)</h3>
                     <h4 className='text-lg'>GITAM University</h4>
                     <h4 className=''>Visakhapatnam</h4>
                     <h5 className=''>2021 - 2025</h5>
                 </div>
-                <div className='bg-gray-700 rounded-3xl w-9/12 min-h-max p-4 flex flex-col items-center sm:w-full'>
-                    <h3 className='text-xl font-semibold'>Intermediate M.P.C</h3>
+                <div className='bg-gray-700 rounded-3xl w-full min-h-max p-4 flex flex-col items-center lg:w-3/12'>
+                    <h3 className='text-xl font-semibold text-yellow-400'>Intermediate M.P.C</h3>
                     <h4 className='text-lg'>Sri Chaitanya Junior College</h4>
                     <h4 className=''>Kakinada</h4>
                     <h5 className=''>2019 - 2021</h5>
                 </div>
-                <div className='bg-gray-700 rounded-3xl w-9/12 min-h-max p-4 flex flex-col items-center sm:w-full'>
-                    <h3 className='text-xl font-semibold'>SSC</h3>
+                <div className='bg-gray-700 rounded-3xl w-full min-h-max p-4 flex flex-col items-center lg:w-3/12'>
+                    <h3 className='text-xl font-semibold text-yellow-400'>SSC</h3>
                     <h4 className='text-lg'>Sri Chaitanya Techno School</h4>
                     <h4 className=''>Kakinada</h4>
                     <h5 className=''>2018 - 2019</h5>
