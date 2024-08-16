a28d12ebda2674cf5f4898efeea00c8e31482d80
chiratla-yuva <chiratlayuva@gmail.com>
2024-08-16 21:23:16 +0530

    changes done

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
+import React from 'react';
+import { FaBriefcase } from 'react-icons/fa';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+const ExperienceDetails = [
+    {
+        role: "Full-Stack Web Development Using MERN Stack",
+        company: "CODEBEAT",
+        duration: "May 2024 - Jul 2024"
+    },
+    {
+        role: "Cyber Risk & Regulatory Launchpad",
+        company: "PwC Acceleration Centers in India",
+        duration: "Feb 2021 - Jun 2024"
+    },
+];
 
 const Experience = () => {
     return (
-        <div className="w-full min-h-screen pt-16 p-6 flex flex-wrap flex-col items-center justify-evenly bg-gray-800" id="experience">
-            <div className='flex flex-col items-center'>
-                <h1 className="text-center text-4xl font-bold text-green-500">Experience</h1>
-                <hr className="w-1/2 mx-auto my-4 border-green-500" />
-                <div className='rounded-3xl w-full min-h-max flex flex-col items-center gap-2'>
-                    <div className='bg-gray-700 rounded-3xl w-full min-h-max p-6 flex flex-col items-center'>
-                        <h3 className='text-xl font-normal'>Full-Stack Web Development Using MERN Stack</h3>
-                        <h4 className='text-md'>CODEBEAT</h4>
-                        <h5 className='text-md'>May 2024 - Jul 2024</h5>
-                    </div>
-                    <div className='bg-gray-700 rounded-3xl w-full min-h-max p-6 flex flex-col items-center'>
-                        <h3 className='text-xl font-normal'>Cyber Risk & Regulatory Launchpad</h3>
-                        <h4 className='text-md'>PwC Acceleration Centers in India</h4>
-                        <h5 className='text-md'>Feb 2021 - Jun 2024</h5>
-                    </div>
-                </div>
-            </div>
-            <div>
-                <h1 className="text-center text-4xl font-bold text-green-500">Certificates</h1>
-                <hr className="w-1/2 mx-auto my-4 border-green-500" />
+        <div className="w-full min-h-screen pt-20 p-4 flex flex-wrap flex-col items-center text-white bg-gray-800" id="experience">
+            <h1 className="text-center text-2xl font-bold text-green-500">Experience</h1>
+            <hr className="w-1/6 my-2 border-green-500" />
+            <VerticalTimeline>
+                {ExperienceDetails.map((exp, index) => (
+                    <VerticalTimelineElement
+                        key={index}
+                        className="vertical-timeline-element--work"
+                        contentStyle={{ background: '#393E46', color: '#fff' }}
+                        contentArrowStyle={{ borderRight: '7px solid  #393E46' }}
+                        date={exp.duration}
+                        iconStyle={{ background: '#00ADB5', color: '#EEEEEE' }}
+                        icon={<FaBriefcase />}
+                    >
+                        <h3 className="vertical-timeline-element-title text-xl font-semibold text-yellow-400">{exp.role}</h3>
+                        <h4 className="vertical-timeline-element-subtitle text-md text-white">{exp.company}</h4>
+                    </VerticalTimelineElement>
+                ))}
+            </VerticalTimeline>
+
+            <div className='mt-4'>
+                <h1 className="text-center text-2xl font-bold text-green-500">Certificates</h1>
+                <hr className="w-1/2 mx-auto my-2 border-green-500" />
                 <div className='rounded-3xl w-full min-h-max p-2 flex flex-col items-center gap-2.5'>
                     <div className='bg-gray-700 rounded-3xl w-full min-h-max p-6 flex flex-col items-start'>
                         <a className='text-lg font-normal' href="http://">Meta Front-End Developer</a>
@@ -31,7 +50,7 @@ const Experience = () => {
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Experience
+export default Experience;
