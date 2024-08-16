1f66ebd31a2476214352537b1b94c75d8cea6281
chiratla-yuva <chiratlayuva@gmail.com>
2024-08-16 21:48:50 +0530

    changes done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,24 +9,31 @@ const Home = () => {
         const hour = date.getHours();
         let currentWish;
 
-        if (hour < 12) {
+        if (hour >= 1 && hour <= 11) {
             currentWish = 'Good Morning,';
-        } else if (hour < 18) {
+        } else if (hour >= 12 && hour <= 15) {
             currentWish = 'Good Afternoon,';
-        } else {
+        } else if (hour >= 16 && hour <= 19) {
             currentWish = 'Good Evening,';
+        } else {
+            currentWish = 'Good Night,';
         }
         setWish(currentWish);
     }, []);
 
     return (
-        <div className="w-full h-screen flex flex-wrap items-center justify-evenly text-white bg-gray-800" id="home">
+        <div className="w-full h-screen flex flex-col-reverse md:flex-row items-center justify-center text-white bg-gray-800 gap-12" id="home">
             <div className="text-center md:text-left">
-                <h2 className="text-2xl md:text-3xl lg:text-4xl mb-4">Hi <span className="text-yellow-400">{wish}</span></h2>
-                <h1 className="text-xl md:text-2xl lg:text-5xl">I am <span className="text-green-400 font-medium">Chiratla.</span><br/>Yuva Sai Manikanta</h1>
+                <h2 className="text-3xl md:text-4xl lg:text-5xl mb-2">
+                    Hi <span className="text-yellow-400">{wish}</span>
+                </h2>
+                <h1 className="text-3xl md:text-4xl lg:text-5xl">
+                    I am <span className="text-green-400 font-medium">Chiratla.</span><br />
+                    Yuva Sai Manikanta
+                </h1>
             </div>
-            <div className="profile">
-                <img src={Logo} alt="Profile Pic" className="w-48 md:w-56 lg:w-72 rounded-full" />
+            <div className="flex-shrink-0">
+                <img src={Logo} alt="Profile Pic" className="w-56 md:w-56 lg:w-72 rounded-full" />
             </div>
         </div>
     );
